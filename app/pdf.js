8783616f5baaa377f384e638293e10b34837fbaa
owtotwo <owtotwo@163.com>
2017-08-09 22:46:48 +0800

    [Fix] jshint error

--- app/pdf.js
+++ app/pdf.js
@@ -1,4 +1,3 @@
-/* global window, MathJax */
 var fs = require('fs');
 var path = require('path');
 var os = require('os');
@@ -9,10 +8,10 @@ exports.export = function (req, res, next) {
 	function onError(err) {
 		next(err);
 	}
-	function onUnknownError() {
-		res.statusCode = 400;
-		res.end('Unknown error');
-	}
+	// function onUnknownError() {
+	// 	res.statusCode = 400;
+	// 	res.end('Unknown error');
+	// }
 	function onTimeout() {
 		res.statusCode = 408;
 		res.end('Request timeout');
@@ -34,7 +33,7 @@ exports.export = function (req, res, next) {
 	var marginLeft = parseInt(options.marginLeft);
 
 
-	var options = {
+	var configs = {
 		"format": "A4",        // allowed units: A3, A4, A5, Legal, Letter, Tabloid 
 		"orientation": "portrait", // portrait or landscape 
 
@@ -47,27 +46,31 @@ exports.export = function (req, res, next) {
 
 		// File options 
 		"type": "pdf",             // allowed file types: png, jpeg, pdf 
-	}
+	};
 
 
 	var filePath = path.join(os.tmpDir(), Date.now() + '.pdf');
 	var tmpFile = fs.createWriteStream(filePath);
 	var pipe = req.pipe(tmpFile);
 
-	pipe.on('finish', () => {
+	pipe.on('finish', function() {
 		console.log("success to write the request content to temp html file: ", filePath);
 		var html = fs.readFileSync(filePath, 'utf8');
-		fs.unlink(filePath, () => console.log("remove the temp html file: ", filePath));
-		pdf.create(html, options).toStream(function (err, stream) {
-			stream.pipe(res);
+		fs.unlink(filePath, function() { console.log("remove the temp html file: ", filePath); });
+		pdf.create(html, configs).toStream(function (err, stream) {
+			if (err) {
+				onError("error in html to pdf process");
+			} else {
+				stream.pipe(res);
+			}
 		});
 	});
 
-	pipe.on('error', () => {
+	pipe.on('error', function() {
 		onError("error to write temp file...");
 	});
 
-	pipe.on('timeout', () => {
+	pipe.on('timeout', function() {
 		onTimeout();
 	});
 };
