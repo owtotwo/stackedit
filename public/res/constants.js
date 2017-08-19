6937765df1179e123aa4b5adf3db6ca1333f9c6d
owtotwo <owtotwo@163.com>
2017-08-19 14:41:24 +0800

    change default couchdb url

--- public/res/constants.js
+++ public/res/constants.js
@@ -28,7 +28,7 @@ define([], function() {
 	constants.PICASA_IMPORT_IMG_URL = "/picasaImportImg";
 	constants.SSH_PUBLISH_URL = '/sshPublish';
 	constants.PDF_EXPORT_URL = "/pdfExport";
-	constants.COUCHDB_URL = 'https://couchdb.sysu.at/document';
+	constants.COUCHDB_URL = 'https://couchdb.sysu.at/documents';
 
 	// Site dependent
 	constants.BASE_URL = "http://localhost/";
